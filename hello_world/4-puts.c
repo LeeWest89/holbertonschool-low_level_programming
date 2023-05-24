094588bfea44eefb8251624af78194466701b0d1
LeeWest89 <6683@holbertonstudents.com>
2023-05-24 15:31:33 -0700

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main(void) 
+/* A simple C Program */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle") ;
 	return (0);
 }
