05f17f64b7c8dd84c8d88041e1033e38850dec69
LeeWest89 <6683@holbertonstudents.com>
2023-05-24 15:52:30 -0700

    Hello, world

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -3,13 +3,13 @@
 /**
  * main - Entry point
  *
- * Description: Prints the message "Programming is like building a multilingual puzzle"
+ * Description: Prints "\"Programming is like building a multilingual puzzle"
  *              using the puts function.
  *
  * Return: Always 0 (indicating successful program execution)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
