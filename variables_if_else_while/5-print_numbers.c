443cf584200934364e622d7262ed233a4db5077a
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 14:14:52 -0700

    VIEW

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Desciption: using a while loop to count 0-9.
+ * Desciption: using a for loop to count 0-9.
  *
  * Return: Always 0 (indicating successful program execution)
  */
