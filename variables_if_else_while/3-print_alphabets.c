fa26af7a40ca76b8ae7adea8435261959514a594
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 13:28:25 -0700

    VIEW

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,29 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using putchar to lower and upercase alphabet
+ *
+ * Return: Always 0 (indicating a successful program execution)
+ */
+
+int main(void)
+{
+	char lowercase;
+	char uppercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
