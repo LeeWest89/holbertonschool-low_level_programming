fe83dab0bb550b886a14473bc2d23a0777c1a364
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 12:52:26 -0700

    VIEW

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return 0;
+}
