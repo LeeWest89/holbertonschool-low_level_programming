32fc54ce03ce442be1912ce5e6c8d2ad82016ada
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 14:57:25 -0700

    VIEW

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,30 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: using a while loop to display single-digit numbers combos
+ *
+ * Return: Always 0 (indicating successful program execution)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		putchar(n = '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
