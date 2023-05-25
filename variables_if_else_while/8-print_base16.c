4b6742daab667546ae58c01cb6a163a614819c9f
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 14:46:34 -0700

    VIEW

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,32 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: using a while loop to display z-a.
+ *
+ * Return: Always 0 (indicating successful program execution)
+ */
+
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	d = 'a';
+
+	while (d <= 'f')
+	{
+		putchar(d);
+		d++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
