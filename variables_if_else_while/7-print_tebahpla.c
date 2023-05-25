355df9593610871e875a8d0b3b0ae05f34613901
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 14:40:11 -0700

    VIEW

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,24 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: using a while loop to display z-a.
+ *
+ * Return: Always 0 (indicating successful program execution)
+ */
+
+int main(void)
+{
+	int l = 'z';
+
+	while (l >= 'a')
+	{
+		putchar(l);
+		l--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
