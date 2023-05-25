38ecd2faee084f1d1c88857ab09ef77a8a452171
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 14:01:44 -0700

    VIEW

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,26 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using putchar to print every letter except 'e' and 'q'.
+ *
+ * Return: Always 0 (indicating successful program execution)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
