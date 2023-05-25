3fe2ffce78fbc773d3c630f813eec0b2d5be0865
LeeWest89 <6683@holbertonstudents.com>
2023-05-25 14:11:38 -0700

    VIEW

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desciption: using a while loop to count 0-9.
+ *
+ * Return: Always 0 (indicating successful program execution)
+ */
+
+int main(void)
+{
+	char number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		putchar(number);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
