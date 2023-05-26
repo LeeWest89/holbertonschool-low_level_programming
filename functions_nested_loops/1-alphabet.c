85d29f10a2675e762f2cbe7fb9084fad599b002d
LeeWest89 <6683@holbertonstudents.com>
2023-05-26 12:03:19 -0700

    functions

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,1 +1,21 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *print_alphabet - Prints the alphabet in lowercase followed be a new line
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+	{
+		_putchar(l);
+		l++;
+	}
+	_putchar('\n');
+}
