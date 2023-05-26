b8c8d57acf6c365922f31fc6c039c1d97380506b
LeeWest89 <6683@holbertonstudents.com>
2023-05-26 12:35:34 -0700

    functions

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,25 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		char l = 'a';
+
+		while (l <= 'z')
+		{
+			_putchar(l);
+			l++;
+		}
+		n++;
+	}
+	_putchar('\n');
+}
