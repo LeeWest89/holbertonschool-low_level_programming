40deda0efe8fa375fb7f5554e4ab6c355055257f
LeeWest89 <6683@holbertonstudents.com>
2023-05-26 13:17:31 -0700

    functions

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _isalpha - checksfor lowercase and uppercase characters
+ *
+ * @c: the character being checked
+ *
+ * Return: 1 for lowercase or uppercase character or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
