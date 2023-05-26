0a411e21ae0eca944140a48197baff52ca47b584
LeeWest89 <6683@holbertonstudents.com>
2023-05-26 12:59:43 -0700

    functions

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase characters
+ *
+ * @c: the charcter being chected
+ *
+ * Return: 1 for lowercase character or 0 for anything else
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+		return (0);
+}
