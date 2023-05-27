8954e5c945af4ee04fd240ac7c6f357b51a9756f
LeeWest89 <6683@holbertonstudents.com>
2023-05-27 15:45:56 -0700

    function

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,1 +1,17 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _abs - the absolute value of an integer
+ *
+ * @n: The intger
+ *
+ * Return: The absolute value of @n
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
