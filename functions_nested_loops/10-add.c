d853b53ac6aea5cfbc25d5d1660332b47eda79cf
LeeWest89 <6683@holbertonstudents.com>
2023-05-28 07:32:47 -0700

    function

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#include "main.h"
+/**
+ * add - adds integers together.
+ * @a: the first integer.
+ * @b: the second integer.
+ *
+ * Return: The sum of the integers.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
