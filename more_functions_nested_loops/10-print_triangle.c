71b8fd6dc49d59487ee03813cbeb53cbfd81081c
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 09:56:45 -0700

    more

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,29 @@
-#!/usr/bin/env bash
+#include "main.h"
+/**
+ * print_triangle - prints a triangle with #.
+ *
+ * @size: the size of the triangle to be printed.
+ */
+void print_triangle(int size)
+{
+	int a = 0;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (a < size)
+	{
+		b = 0;
+		while (b <= size)
+		{
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
