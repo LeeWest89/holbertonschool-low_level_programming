8fe8f79794270c0f5ece8eab558bae32be71206c
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 09:25:38 -0700

    more

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,1 +1,29 @@
-#!/usr/bin/env bash
+#include "main.h"
+/**
+ * print_square - prints a square with #.
+ *
+ * @size: the size of the square to be printed.
+ */
+void print_square(int size)
+{
+	int a = 0;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (a < size)
+	{
+		b = 0;
+		while (b < size)
+		{
+			_putchar('#');
+			b++;
+		}
+		_putchar('\n');
+		a++;
+	}
+}
