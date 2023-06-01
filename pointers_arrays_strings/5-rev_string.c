8e6c0767c6c804fc8b84d36bc4d548fefdc26550
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 14:48:28 -0700

    PAS

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,29 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * rev_string - Reverses the string.
+ * @s: String to be reverse.
+ */
+
+void rev_string(char *s)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d;
+
+	while (s[a] != '\0')
+		a++;
+
+	c = a - 1;
+
+	while (b < c)
+	{
+		d = s[b];
+		s[b] = s[c];
+		s[c] = d;
+
+		b++;
+		c--;
+	}
+}
