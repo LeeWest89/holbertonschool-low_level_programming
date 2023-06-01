1d2fc53307a5d6215a80991591f93434242e7b74
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 08:37:00 -0700

    more

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,17 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * print_most_numbers - prints most numbers
+ */
+
+void print_most_numbers(void)
+{
+	char numbers[] = "01356789\n";
+	int n = 0;
+
+	while (n < 9)
+	{
+		_putchar(numbers[n]);
+		n++;
+	}
+}
