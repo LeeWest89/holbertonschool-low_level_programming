4719f78c4d909bdc77ddfb002a73b3825209f510
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 08:20:12 -0700

    more

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	n = 0;
+
+	while (n >= 0)
+	{
+		_putchar(n);
+	}
+	(n++);
+	_putchar('\n');
+}
