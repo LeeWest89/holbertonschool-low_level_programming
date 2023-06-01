ce6ef90bf41b49099c8e9ef7f42758098808c908
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 08:51:09 -0700

    more

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,25 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14
+ */
+
+void more_numbers(void)
+{
+	int a = 0;
+	int n;
+
+	while (a < 10)
+	{
+		n = 0;
+		while (n <= 14)
+		{
+			if (n > 9)
+				_putchar('0' + n / 10);
+			_putchar('0' + n % 10);
+			n++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
