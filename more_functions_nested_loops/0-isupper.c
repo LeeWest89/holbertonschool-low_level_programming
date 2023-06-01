0ab1ce93b6e40c62e6275e39cc9b9319362a2c49
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 07:41:04 -0700

    more

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,8 @@
 
 /**
  * _isupper - Checks for capital letters
- * @c: the letter being checked 
+ * @c: the letter being checked
+ * Return: 1 on captial C, 0 on anything else
  */
 
 int _isupper(int c)
