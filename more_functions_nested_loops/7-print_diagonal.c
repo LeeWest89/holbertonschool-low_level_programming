1a8d7432af09bc6aff2c7278140c88330fdf192d
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 09:17:34 -0700

    more

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,1 +1,30 @@
-#!/usr/bin/env bash
+#include "main.h"
+/**
+ * print_diagonal - prints a line of diagonals
+ *
+ * @n: the number of diagonals to be printed.
+ */
+void print_diagonal(int n)
+{
+	int a = 0;
+	int b;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	while (a < n)
+	{
+		b = 0;
+		while (b < a)
+		{
+			_putchar(' ');
+			b++;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		a++;
+	}
+}
