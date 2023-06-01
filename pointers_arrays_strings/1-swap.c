0b26c69434a0be16594b3864a360c827f5cdfa42
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 13:44:43 -0700

    PAS

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * swap_int - swaps the vaule of integer a and b.
+ *
+ * @a: the pointer to be swapped with b.
+ *
+ * @b: the pointer to be swapped with a.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
