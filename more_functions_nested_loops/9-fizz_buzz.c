c384c91be01df5e8188bfe7fe90880b192d5f56c
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 09:39:31 -0700

    more

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,1 +1,37 @@
-#!/usr/bin/env bash
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 0n success
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+
+		n++;
+	}
+
+	printf("\n");
+	return (0);
+}
