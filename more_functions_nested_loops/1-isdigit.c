75a217ea2907beb43ea1bd9413fe8523962fbb12
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 07:53:51 -0700

    more

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _isdigit - CHecks if c is a digit
+ *
+ * @c: the char being checked
+ *
+ * Return: 1 if it is a digit, 0 if anything else
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
