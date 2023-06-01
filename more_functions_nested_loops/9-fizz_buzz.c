4e6266b492bf8072ecf88f8645b3d3fbb23c8409
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 09:50:01 -0700

    more

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,28 +9,36 @@
 int main(void)
 {
 	int n = 1;
+	int a = 0;
 
 	while (n <= 100)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf("%d", n);
+		}
+
+		a++;
+
+		if (a < 100)
+		{
+			printf(" ");
 		}
 
 		n++;
 	}
-
+	printf("\n");
 	return (0);
 }
