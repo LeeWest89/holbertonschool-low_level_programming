1ec2b7668465807bb88f0a8e0b1d1358d4e5feb5
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 09:01:36 -0700

    more

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+#include "main.h"
+/**
+ * print_line - prints a line of underscores
+ *
+ * @n: the number of underscores to be printed.
+ */
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	while (a < n)
+	{
+		_putchar('_');
+		a++;
+	}
+
+	_putchar('\n');
+}
