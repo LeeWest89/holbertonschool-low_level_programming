976aefa2ad333a529c7b2136c3c5d5563130196b
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 08:04:17 -0700

    more

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,1 +1,16 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * mul - multiples to integers together
+ *
+ * @a: the first integer
+ *
+ * @b: the seconf integer
+ *
+ * Return: result of multiplying a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
