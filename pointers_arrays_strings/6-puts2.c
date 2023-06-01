5153e9a76833daf65c0b033fc2fd9c7c4691d1cc
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 15:13:49 -0700

    PAS

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _puts2 - Prints every other character of a string.
+ * @str: String to print
+ */
+
+void _puts2(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+		_putchar(str[s]);
+		s += 2;
+	}
+
+	_putchar('\n');
+}
