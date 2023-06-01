93a0f01201d09a9c85612188a249d1d5bf6fab58
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 14:18:46 -0700

    PAS

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _puts - Prints a string.
+ * @str: String to print
+ */
+
+void _puts(char *str)
+{
+	int s = 0;
+
+	while (str[s] != '\0')
+	{
+		_putchar(str[s]);
+		s++;
+	}
+
+	_putchar('\n');
+}
