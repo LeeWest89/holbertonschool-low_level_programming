83f621a269ac0e75e9ceffdbddb1d1bbdc487089
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 13:39:47 -0700

    PAS

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,12 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value poited to by a pointer to 98.
+ *
+ * @n: Pointer to an integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
