c6022f37a404c98075e40730576e6add071420ad
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 14:03:34 -0700

    PAS

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,20 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: the input string.
+ * Return: the length of the string.
+ */
+
+int _strlen(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0)
+	{
+		l++;
+		s++;
+	}
+
+	return l;
+}
