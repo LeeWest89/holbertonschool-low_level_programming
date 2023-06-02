5d35c3da91f6fd3f6f9cf5c5bd55c47c167ea105
LeeWest89 <6683@holbertonstudents.com>
2023-06-02 08:03:56 -0700

    PAS

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,29 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _atoi - Makes a string and integer
+ * @s: The string to convert
+ *
+ * Return: the converted integer
+ */
+
+int _atoi(char *s)
+{
+	int a = 1;
+	int b = 0;
+	int c = 0;
+
+	while (s[c])
+	{
+		if (s[i] == '-')
+			a *= -1;
+		if (s[c] >= '0' && s[c] <= '9')
+		{
+			b = b * 10 + (s[c] - '0');
+			if (s[c + 1] < '0' || s[c + 1] > '9')
+				break;
+		}
+		c++;
+	}
+	return (b * a);
+}
