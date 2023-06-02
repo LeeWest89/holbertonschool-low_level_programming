91d279f3000430e0bd9072bb8f0d29807a79309b
LeeWest89 <6683@holbertonstudents.com>
2023-06-02 07:00:59 -0700

    PAS

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,24 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * print_ array - prints the number associated with the array.
+ * @*a: the array
+ * @n: the number from the array
+ */
+
+void print_array(int *a, int n)
+{
+	int b = 0;
+
+	while (b < n)
+	{
+		printf("%d", a[b]);
+
+		if (b != n - 1)
+			printf(", ");
+
+		b++;
+	}
+
+	printf("\n");
+}
