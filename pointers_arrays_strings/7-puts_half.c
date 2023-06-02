a2affd2aaebb6f7eee503867ba9523176d7a97a2
LeeWest89 <6683@holbertonstudents.com>
2023-06-01 17:06:04 -0700

    PAS

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,24 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string.
+ * @str: String to print.
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+	int n;
+	int a;
+
+	while (str[l] != '\0')
+		l++;
+
+	if (l % 2 == 0)
+		n = l / 2;
+	else
+		n = (l - 1) / 2;
+	for (a = n; a < l; a++)
+		_putchar(str[a]);
+
+	_putchar('\n');
+}
