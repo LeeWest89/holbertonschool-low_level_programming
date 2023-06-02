4ef849751b51e36f38971767b8ebf979462f5cfb
LeeWest89 <6683@holbertonstudents.com>
2023-06-02 07:35:37 -0700

    PAS

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,23 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _strcpy - Copies the string from src to the buffer dest.
+ * @dest: Strings destination
+ * @src: Source string
+ *
+ * Return: pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
