2dde947a3f0905166213aaff0fb90652e3672a09
LeeWest89 <6683@holbertonstudents.com>
2023-06-07 07:34:36 -0700

    PAS

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,30 +1,29 @@
 #include "main.h"
-
 /**
- * _strncpy - copies a string
- * @dest: Destination
- * @src: String
- * @n: number of bytes to copy
+ * _strncat - cats two strings
+ * @dest: the destination string.
+ * @src: the source string
+ * @n: max bytes from src to cat
  *
- * Return: Pointer to the destination
+ * Return: pointer to the resulting string 'dest'.
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	int a = 0;
+	char *a = dest;
 
-	while (a < n)
-	{
-		dest[a] = src[a];
-
-		if (src[a] == '\0')
-			break;
-
+	while (*a)
 		a++;
+
+	while (*src && n > 0)
+	{
+		*a = *src;
+		a++;
+		src++;
+		n--;
 	}
 
-	for (; a < n; a++)
-		dest[a] = '\0';
+	*a = '\0';
 
 	return (dest);
 }
