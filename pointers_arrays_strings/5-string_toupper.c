db5cba6324bc8bcfa0dc490e3f8e1fe08cbd2bcc
LeeWest89 <6683@holbertonstudents.com>
2023-06-07 08:25:25 -0700

    PAS

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,24 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * string_toupper - converts lowercase letters to uppercase letters in a string
+ * @str: the string to convert
+ *
+ * Return: pointer to converted string.
+ */
+
+char *string_toupper(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (str[a] >= 'a' && str[a] <= 'z')
+		{
+			str[a] = str[a] - ('a' - 'A');
+		}
+		a++
+	}
+
+	return (str);
+}
