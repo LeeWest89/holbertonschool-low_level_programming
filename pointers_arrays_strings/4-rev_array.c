5845e91c19cc68cee205b3f352d83835ef789dd5
LeeWest89 <6683@holbertonstudents.com>
2023-06-07 08:03:53 -0700

    PAS

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,19 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * reverse_array - prints an array of integers in reverse
+ * @a: array of integers
+ * @n: the number of elements to swap
+ */
+
+void reverse_array(int *a, int n)
+{
+	int b, c;
+
+	for (b = 0; b < n / 2; b++)
+	{
+		c = a[b];
+		a[b] = a[n - 1 - b];
+		a[n - 1 - b] = c;
+	}
+}
