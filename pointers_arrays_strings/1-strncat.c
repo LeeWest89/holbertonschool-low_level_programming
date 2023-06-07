d6191143acbbe283462545a2bdc324b16271b6e9
LeeWest89 <6683@holbertonstudents.com>
2023-06-07 07:28:09 -0700

    PAS

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,30 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: Destination
+ * @src: String
+ * @n: number of bytes to copy
+ *
+ * Return: Pointer to the destination
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int a = 0;
+
+	while (a < n)
+	{
+		dest[a] = src[a];
+
+		if (src[a] == '\0')
+			break;
+
+		a++;
+	}
+
+	for (; a < n; a++)
+		dest[a] = '\0';
+
+	return (dest);
+}
