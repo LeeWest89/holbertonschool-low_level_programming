202bfdecc1a7ccca28ca5fbd09ca1dd4da977f55
LeeWest89 <6683@holbertonstudents.com>
2023-06-08 11:09:00 -0700

    PAS

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,30 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _strpbrk - checks string for certain character
+ * @s: the string being searched
+ * @accept: the character being searched for
+ *
+ * Return: pointer to the byte in s that matches accept, or NULL.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int a, b;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; accept[b] != '\0'; b++)
+		{
+			if (s[a] == accept[b])
+			{
+				return (s);
+			}
+		}
+
+		if (accept[b] == '\0')
+		break;
+	}
+
+	return ("");
+}
