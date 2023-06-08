6d59b4dfd6dd98657957933345313da9d0bb1808
LeeWest89 <6683@holbertonstudents.com>
2023-06-08 15:14:48 -0700

    PAS

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,21 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the array
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int w, b;
+
+	for (w = 0; w < 8; w++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[w][b]);
+		}
+
+		_putchar('\n');
+	}
+}
