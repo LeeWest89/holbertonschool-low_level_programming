5c392899eaff87954d2bf4f3326561ef188ecdf2
LeeWest89 <6683@holbertonstudents.com>
2023-06-08 07:42:59 -0700

    PAS

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
+ * _strchr - locates a character in a string
+ * @s: the string being searched
+ * @c: the character being searched for
+ *
+ * Return: pointer th for occurrence of c, or NULL if characteris not found.
  */
+
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; a < s; a++)
+		if (s[a] == c)
+			return (s);
+		else if (s[a] == '\0')
+			return (NULL);
+}
