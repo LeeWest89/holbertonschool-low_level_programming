00ebe04345cdfc5e51811c90125df3bfba93d7dc
LeeWest89 <6683@holbertonstudents.com>
2023-06-08 14:19:55 -0700

    PAS

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -18,16 +18,10 @@ char *_strpbrk(char *s, char *accept)
 		for (b = 0; accept[b] != '\0'; b++)
 		{
 			if (s[a] == accept[b])
-				return (s);
-		}
-
-		{
-
-			if (s[a] != accept[b])
-				return (NULL);
+				return (s[a]);
 		}
 
 	}
 
-	return (0);
+	return (NULL);
 }
