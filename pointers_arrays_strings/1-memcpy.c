c2971a401ef23c8fb78063f2d6b89654a1937f82
LeeWest89 <6683@holbertonstudents.com>
2023-06-08 07:14:13 -0700

    PAS

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,20 @@
 #include "main.h"
 
 /**
+ * _memcpy - copies memory from src to dest.
+ * @dest: Memory area to recieve copy
+ * @src: Memory to be copied
+ * @n: Number of bytes to be copied
+ *
+ * Return: dest
  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src;
+
+	return (dest);
+}
