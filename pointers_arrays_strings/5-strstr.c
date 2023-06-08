7e2cb8a91b89ae33c2bdd1e38c44e4ac2ab93ce7
LeeWest89 <6683@holbertonstudents.com>
2023-06-08 14:37:41 -0700

    PAS

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,31 @@
-#!/usr/bin/env bash
+#include "main.h"
+#include <stddef>
+
+/**
+ * _strstr - checks string for certain substring
+ * @haystack: the string being searched
+ * @needle: the substring being searched for
+ *
+ * Return: substring if present or NULL if not present
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a] == needle[b])
+			{
+				return (haystack + a);
+			}
+		}
+
+		if (needle[b] == '\0')
+		break;
+	}
+
+	return (NULL);
+}
