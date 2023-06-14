a65a1047fb5983d263dcf96a479414c0f546bdd1
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 09:07:02 -0700

    Recursion

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,1 +1,21 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse.
+ * @s: the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	++s;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+
+}
