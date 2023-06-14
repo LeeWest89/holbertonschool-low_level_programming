ee9c55a223cdd5536c3fa8040dbaf6b842f3c8a4
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 11:41:19 -0700

    Recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * is_prime_number - tell you if number is prime.
+ * @n: the number
+ *
+ * Return: 1 if prime or 0 otherwise
+ */
+
+int is_prime_number(int n)
+
+{
+	if ((n % 2 == 0) || (n % 3 == 0))
+		return (0);
+
+	if ((n % 2 != 0) || (n % 3 != 0))
+		return (1);
+}
