442e17b760000382f5f871e911ea1ccb35b48232
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 08:17:15 -0700

    Recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints the string
+ * @s: the string
+ */
+
+void _puts_recursion(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		_putchar(s);
+}
