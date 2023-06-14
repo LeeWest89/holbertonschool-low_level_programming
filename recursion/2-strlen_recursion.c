637e129d100b05599dcc51f81561c40f9f87fe62
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 09:32:16 -0700

    Recursion

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,1 +1,22 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _strlen_recursion - prints the length of the string
+ * @s: the string
+ *
+ * Return: 0 if string is '\0' or length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (s + 1);
+
+
+}
