fff01d003965bfee51cc0dd7ab3809eeebcb5674
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 10:16:54 -0700

    Recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,1 +1,27 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _pow_recursion - multiples x to the recursion of y
+ * @x: the number
+ * @y: the number of factorial to x
+ *
+ * Return: -1 is y is < 0, returns the factorial of x by power y.
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	if ((y == 0) || (y == 1))
+	{
+		return (x * 1);
+	}
+
+	return (x * factorial(y - 1));
+
+
+}
