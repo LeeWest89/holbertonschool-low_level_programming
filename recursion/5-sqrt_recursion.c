389509c89b511dc5909a999da6db920692ecfc28
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 10:57:49 -0700

    Recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,1 +1,28 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns square root of a number
+ * @n: the number
+ *
+ * Return: -1 if n does not have a square root or the square root of n
+ */
+
+int _sqrt_recursion(int n)
+{
+	int a, i;
+
+	{
+		int a = i * i;
+
+		if (a > n)
+			return (-1);
+
+		if (a == n)
+			return (i);
+
+		return (a(n, i + 1));
+	}
+
+	return (a(n, 1));
+}
+
