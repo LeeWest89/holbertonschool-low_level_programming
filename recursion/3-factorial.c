cc42878594158c37cd36f363aaa5c96c162bcd83
LeeWest89 <6683@holbertonstudents.com>
2023-06-14 10:01:02 -0700

    Recursion

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,1 +1,26 @@
-#!/usr/bin/env bash
+#include "main.h"
+
+/**
+ * factorial - multiple the given number factorially to !0.
+ * @n: the number
+ *
+ * Return: -1 is n is < 0, 1 if n is 0 or 1, returns the factorial of n.
+ */
+
+int factorial(int n)
+{
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if ((n == 0) || (n == 1))
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+
+
+}
