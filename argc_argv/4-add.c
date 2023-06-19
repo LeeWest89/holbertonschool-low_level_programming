e38d548d1b19fdb7521a4a8705cac9327f3d2be2
LeeWest89 <6683@holbertonstudents.com>
2023-06-19 10:07:49 -0700

    argc, argv[]

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,32 @@
 #include "main.h"
-#include <libmy.a>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
+ * main - the enter point.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ *
+ * Return: 0 on success.
  */
+
+int main(int argc, char *argv[])
+{
+	int a, sum = 0;
+
+	if (argc > 1)
+	{
+		for (a = 1; a < argc; a++)
+		{
+			sum += atoi(argv[a]);
+		}
+		printf("%d\n", sum)
+	}
+
+	else if (argv[a] < 0)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
