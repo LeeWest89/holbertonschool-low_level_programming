161476b9ca0824ae482ae3e763940617862b4646
LeeWest89 <6683@holbertonstudents.com>
2023-06-19 10:47:37 -0700

    argc, argv[]

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -14,17 +14,20 @@ int main(int argc, char *argv[])
 {
 	int a, b, sum = 0;
 
-	for (a = 1; a < argc, a++)
+	for (a = 1; a < argc; a++)
 	{
 		for (b = 0; argv[a][b]; b++)
 		{
 			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
 				printf("Error\n");
 				return (1);
+			}
 		}
 
 		sum += atoi(argv[a]);
 	}
 
+	printf("%d\n", sum)"
 	return (0);
 }
