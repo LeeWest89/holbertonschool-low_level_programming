85947e864e31c1d74d7069e566cdad41cf35d150
LeeWest89 <6683@holbertonstudents.com>
2023-06-19 09:03:01 -0700

    argc, argv[]

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - the enter point.
+ * @argc: argument counter.
+ *
+ * Return: 0 on success.
+ */
+
+int main(int argc)
+{
+	printf("%d", argc);
+	return (0);
+}
