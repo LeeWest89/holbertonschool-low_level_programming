89fcba10c6185fdb59b3887389c0f0bb4f7ba464
LeeWest89 <6683@holbertonstudents.com>
2023-06-19 09:41:09 -0700

    argc, argv[]

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,28 @@
 #include "main.h"
-#include <libmy.a>
+#include <stdio.h>
 
 /**
+ * main - the enter point.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ *
+ * Return: 0 on success.
  */
+
+int main(int argc, char *argv[])
+{
+	int a = argv[1], b = argv[2];
+
+	if (argc == 3)
+	{
+		printf("%d\n", a * b);
+	}
+
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
