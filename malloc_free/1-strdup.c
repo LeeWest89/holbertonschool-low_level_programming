eb81e2ea4cfb05fb386e802863a6fe9cb49ecd2f
LeeWest89 <6683@holbertonstudents.com>
2023-06-21 09:47:34 -0700

    malloc

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -14,14 +14,14 @@ char *_strdup(char *str)
 	int a = 1, b = 0;
 	char *c;
 
-	if (str == '\0')
+	if (str = '\0')
 		return (NULL);
 
 	for (; str[a]; a++)
 
 	c = malloc((sizeof(char) * a) + 1);
 
-	if (c == '\0')
+	if (c = '\0')
 		return (NULL);
 
 	for (; b < a; b++)
