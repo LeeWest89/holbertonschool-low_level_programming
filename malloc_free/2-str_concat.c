cb90fb041be38b40e8cb6fc4bc24c30b4a90adf7
LeeWest89 <6683@holbertonstudents.com>
2023-06-21 11:00:19 -0700

    malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,35 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
+ * str_concat - contenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: Return NULL on empty str and on failure.
  */
+
+char *str_concat(char *s1, char *s2)
+{
+	int a, b, size1 = strlen(s1), size2 = strlen(s2);
+	char *c;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (a = 0; a < size1; a++)
+	{
+		c = s1[a];
+		return (c);
+	}
+
+	for (b = 0; b < size2; b++)
+	{
+		c = s2[b];
+		return (c);
+	}
+}
