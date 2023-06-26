64ad6ee5b01369f8fd5f0098730a1bfb7d9a8316
LeeWest89 <6683@holbertonstudents.com>
2023-06-25 19:15:47 -0700

    More Malloc and free

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -3,4 +3,29 @@
 #include <stdlib.h>
 
 /**
+ * array_range - arranges the array min to max
+ * @min: the start of the array
+ * @max: the end of the array
+ *
+ * Return: the pointer or NULL for min > max or failure
  */
+
+int *array_range(int min, int max)
+{
+	int *a, b, c;
+
+	if (min > max)
+		return (NULL);
+
+	b = max - min + 1;
+
+	a = malloc(b * sizeof(int));
+
+	if (a == 0)
+		return (NULL);
+
+	for (c = 0; c < b; c++)
+		a[c] = min++;
+
+	return (a);
+}
