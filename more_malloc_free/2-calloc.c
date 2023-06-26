9e91fa413ad360967f8f40675190c10e3beb937e
LeeWest89 <6683@holbertonstudents.com>
2023-06-25 18:44:37 -0700

    More Malloc and free

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -3,4 +3,26 @@
 #include <stdlib.h>
 
 /**
+ * _calloc - allocates the memory of an array
+ * @nmemb: the elements of the array
+ * @size: the size of the array
+ *
+ * Return: the pointer
  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	if (nmemb == 0)
+		return (NULL);
+
+	if (size == 0)
+		return (NULL);
+
+	a = malloc(size * nmemb);
+
+	if (a == 0)
+		return (NULL);
+
+	return (a);
+}
