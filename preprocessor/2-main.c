b9ae22904a393e927648600dbb4bd045e797dc84
LeeWest89 <6683@holbertonstudents.com>
2023-06-26 12:19:14 -0700

    preprocessor

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /*
- * main - prints the name of the file it was compiled from, and a new line.
+ * main - prints the name of the file it was compiled from
  *
- * Returns: Always 0
+ * Return: Always 0
  */
 
 int main(void)
