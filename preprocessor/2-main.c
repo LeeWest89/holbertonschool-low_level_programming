8977f161ddb324d36ebcbcaae0c7b432a1c1f0a3
LeeWest89 <6683@holbertonstudents.com>
2023-06-26 11:49:06 -0700

    preprocessor

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
- * main - prints the name of the file it was compiled from, and a new line.
+ * main - point of entry for the program
+ * prints the name of the file it was compiled from, and a new line.
  *
  * Returns: Always 0
  */
