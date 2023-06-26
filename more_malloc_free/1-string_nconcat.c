55262df55a7fed728bfc8134d62d516f42e13a17
LeeWest89 <6683@holbertonstudents.com>
2023-06-25 18:18:45 -0700

    More Malloc and free

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,43 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
+ * string_nconcat - contenates two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: Number of bytes of s2 to be concat.
+ *
+ * Return: Return NULL on empty str and on failure. On success s1 +  s2.
  */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *a;
+	int b = 0, d, e;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[b] != '\0')
+		b++;
+
+	a = malloc((b + n + 1) * sizeof(char));
+
+	if (a == NULL)
+		return (NULL);
+
+	for (d = 0; d < b; d++)
+		a[d] = s1[d];
+
+	for (e = 0; e < n; e++)
+		a[d + e] = s2[e];
+
+	a[d + e] = '\0';
+
+	return (a);
+}
