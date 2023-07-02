7d0b22228125acc4932544487e7d0b8e354dec6d
LeeWest89 <6683@holbertonstudents.com>
2023-07-01 19:46:03 -0700

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,1 +1,52 @@
-#include
+#include <libmy.a>
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - New dog info and store name and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: Info of dog or NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner);
+{
+	int n = 0, o = 0, *dog;
+
+	if (name != NULL && owner != NULL)
+	{
+		n = _strlen(name) + 1;
+		o = _strlen(owner) + 1;
+		dog = malloc(sizeof(dog_t));
+
+		if (dog == NULL)
+			return (NULL);
+
+		dog->name = malloc(sizeof(char) * n);
+
+		if (dog->name == NULL)
+		{
+			free(dog);
+			return (NULL);
+		}
+
+		dog->owner = malloc(sizeof(char) * o);
+
+		if (dog->owner == NULL)
+		{
+			free(dog);
+			free(dog->name);
+			return (NULL);
+		}
+
+		dog->name = _strcpy(dog->name, name);
+		dog->owner = _strcpy(dog->owner, owner);
+		dog->age = age;
+
+	}
+
+	return (dog);
+}
