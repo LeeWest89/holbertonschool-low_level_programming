b3a5d6f1472243a47ec25376938abc3cbca9e20f
LeeWest89 <6683@holbertonstudents.com>
2023-07-01 22:39:50 -0700

    3 test

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,1 +1,27 @@
-#include
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**get _op_func - returns a pointer to the function that corresponds
+ *		to the operator given as a parameter.
+ * @s: operator passed as argument to the program
+ *
+ * Returns: pointer to the the operator for the matchin function
+ */
+
+int (*get_op_func(char *s))(int, int);
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i] != NULL && ops[i] != *s)
+		i++;
+
+	return (ops[i]);
+}
