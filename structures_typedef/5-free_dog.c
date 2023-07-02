155da6e2c0112e5987094b3f059ca10836bd7cca
LeeWest89 <6683@holbertonstudents.com>
2023-07-01 20:08:37 -0700

    free dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -1,1 +1,15 @@
-#include
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free_dog - frees info about dogs
+ * @d: the dog
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
