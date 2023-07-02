ef8fd47d189b0624c9a50464872071bfa7e70246
LeeWest89 <6683@holbertonstudents.com>
2023-07-01 21:50:37 -0700

    2.0

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,1 +1,25 @@
-#include
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of the first element for which the cmp function
+ * @array: the array
+ * @size: size of the array
+ * @cmp: pointer to the function to be used to compare values
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+	{
+		if (cmp(array[a]) != 0)
+			return (a);
+	}
+}
