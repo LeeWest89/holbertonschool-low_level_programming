d76fdf0712e9ce92c589bc5f1a89ce7e78027dd5
LeeWest89 <6683@holbertonstudents.com>
2023-07-03 07:57:59 -0700

    1.0.2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -12,7 +12,7 @@
  * Return: if separator is NULL print nothing, otherwise print string or nil is string is NULL
  */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list a;
 
