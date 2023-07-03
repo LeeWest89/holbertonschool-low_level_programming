b7f09897e8d3bbc02555bcbcf76aac60b102795a
LeeWest89 <6683@holbertonstudents.com>
2023-07-03 07:22:53 -0700

    1.0

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -1,7 +1,22 @@
 #include "variadic_functions.h"
-#include <stdargs.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
+ * print_numbers - Returns sum of all parameters
+ * @separator: string to print between Numbers
+ * @n: number of parameters
+ * @...: the parameters
+ *
+ * Return: if separator is NULL print nothing, otherwise print numbers
  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	if (separator == NULL)
+		return;
+
+	else
+		printf("%d/n", ...);
+}
