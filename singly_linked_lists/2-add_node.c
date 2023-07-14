584e0f3cab29292a47c94b2a32ad91ebcc253147
LeeWest89 <6683@holbertonstudents.com>
2023-07-13 22:16:54 -0700

    lets see if i added a node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,1 +1,29 @@
-#include
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the list
+ * @head: a pointer to the head of the list
+ * @str: the node to be added
+ *
+ * Return: NULL on failure or address of new element on success
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (head != NULL && str != NULL)
+	{
+		new = malloc(sizeof(list_t));
+
+		if (new == NULL)
+			return (NULL);
+
+		new->str = strdup(str);
+		new->length = strlen(str);
+		new->next = *head;
+
+		*head = new;
+	}
+	return (new);
+}
