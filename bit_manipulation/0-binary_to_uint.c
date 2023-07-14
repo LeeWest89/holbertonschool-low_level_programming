fa6d003e03634266cdf06de75dc6e9ef0494f4da
LeeWest89 <6683@holbertonstudents.com>
2023-07-14 09:41:50 -0700

    still trying to fix null

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,13 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int result = 0, ptwo = 1, length = strlen(b);
 	int a;
 
+	if (b == '\0')
+		return (NULL);
+
 	for (a = length - 1; a >= 0; a--)
 	{
 		if (b[a] != '0' && b[a] != '1')
-			return (NULL);
+			return (0);
 
 		if (b[a] == '1')
 			result += ptwo;
