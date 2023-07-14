f3fc930e838eec08a1b1b095cf3c63ffdab30675
LeeWest89 <6683@holbertonstudents.com>
2023-07-14 10:39:09 -0700

    simplified from the mask examples i saw

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -1,1 +1,21 @@
-#include
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: the bit
+ * @index: he index, starting from 0 of the bit you want to get
+ *
+ * Return: -1 on an error orthe bit vaule of the index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	if ((n &(1 << index)) == 0)
+		return (0);
+
+	return (1);
+}
