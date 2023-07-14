2f51ed7b833e0c35168970a111084fa7eabd793c
LeeWest89 <6683@holbertonstudents.com>
2023-07-13 21:42:13 -0700

    Number of elements in the linked list

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,1 +1,20 @@
-#include
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list.
+ * @h: the linked list.
+ *
+ * Return: number of elements of the list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		h = h->next;
+		num++;
+	}
+	return (num);
+}
