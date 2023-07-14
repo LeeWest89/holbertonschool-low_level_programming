90cfe4837f85aa38b22007ccee958e6be635c1b6
LeeWest89 <6683@holbertonstudents.com>
2023-07-14 11:19:32 -0700

    lets see if it flips

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,1 +1,22 @@
-#include
+#include "main.h"
+
+/**
+ * flip_bit - returns the number of bits you would need to flip
+ * 	to get from one number to another
+ * @n: the number
+ * @m: the number being flipped to
+ *
+ * Return: bits to flip n to m
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int mask = n ^ m, count = 0;
+
+	while (mask > 0)
+	{
+		count += (mask & 1);
+		mask >>= 1;
+	}
+	return(count);
+}
