d021821039cec7ce6aa0a7c118acb96866399ccb
LeeWest89 <6683@holbertonstudents.com>
2023-07-13 22:38:30 -0700

    lets see if its free

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,1 +1,19 @@
-#include
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
