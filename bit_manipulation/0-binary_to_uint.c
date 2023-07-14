7c1aa504c68aab47d78701b0a55d7d19c2143271
LeeWest89 <6683@holbertonstudents.com>
2023-07-14 09:36:10 -0700

    see if i can make binary into unsigned int

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: the string of binary
+ *
+ * Return: the unsigned int that represents the sting
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0, ptwo = 1, length = strlen(b);
+	int a;
+
+	for (a = length - 1; a >= 0; a--)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+		
+		if (b[a] == '1')
+			result += ptwo;
+
+		ptwo *= 2;
+	}
+	return (result);
+}
