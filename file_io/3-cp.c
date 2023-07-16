e25795a11f6497a0bb51112b1855d55e996f8f33
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 22:11:24 -0700

    lets see if it copies or if i missed an error call

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,1 +1,71 @@
-#include
+#include "main.h"
+
+/**
+ * copying - copies file1 to file2
+ * @file1: the file to be copied
+ * @file2: the file being copied to
+ *
+ * Return: 98, 99 ,100 on an error
+ */
+
+void copying(const char *file1, const char *file2)
+{
+	char buff[1024];
+	int o = open(file1, O_RDONLY);
+	int t = open(file2, O_CREAT | O_WRONLY | TRUNC, 0664);
+	int r = read(o, buff, 1024);
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+		exit(98);
+	}
+
+	if (!file1 || o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+		exit(98);
+	}
+
+	while (r > 0)
+	{
+		if (write(t, buff, r) != r || t == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
+			exit(99);
+		}
+	}
+
+	if (close(o) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", o);
+		exit(100);
+	}
+
+	if (close(t) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", t);
+		exit(100);
+	}
+
+}
+
+/**
+ * main - the entry point
+ * @argc:the number of arguements
+ * @argv: array of pointers to the arguments
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	copying(argv[1], argv[2]);
+	exit(0);
+}
