75ce5e98a86c172b341d3b8b38c52c40bb48c402
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 20:55:46 -0700

    seems to simple lets see

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,1 +1,31 @@
-#include
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file.
+ * @filename: the file
+ * @text_content: text to copy
+ *
+ * Return: -1 if filename is NULL or fails and 1 on success
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int o, a;
+
+	o = open(filename, O_WRONLY | O_APPEND);
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (a = 0; text_content[a]; a++)
+			write(o, &text_content[a], 1);
+	}
+
+	if (o == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
