0afe630bda0f9c41ebb8595e7e565654caceb29b
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 20:32:40 -0700

    forgot the permissions

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 		for (a = 0; text_content[a]; a++)
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, a);
 
 	if (w == -1 || o == -1)
