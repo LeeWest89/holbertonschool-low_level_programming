d7da69ac5e436bac8c2ec536e1528c888c2d36a9
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 20:45:21 -0700

    fix int o placement i hope

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -12,6 +12,8 @@ int create_file(const char *filename, char *text_content)
 {
 	int o, a;
 
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
 	if (filename == NULL)
 		return (-1);
 
@@ -20,8 +22,6 @@ int create_file(const char *filename, char *text_content)
 		for (a = 0; text_content[a]; a++)
 			write(o, text_content, a);
 	}
-
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (o == -1)
 		return (-1);
