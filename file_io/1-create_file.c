cdf13ce57b2cd60588b9062d9fb93cbed28e64f9
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 20:42:22 -0700

    forgot ;

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (a = 0; text_content[a]; a++)
-			write(o, text_content, a)
+			write(o, text_content, a);
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
