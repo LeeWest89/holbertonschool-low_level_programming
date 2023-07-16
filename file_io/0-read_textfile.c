08af7189c1eb832b1614a44e5e0944b6ffb0c3ec
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 19:59:31 -0700

    first try lets see what the complier says

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * read_textfile - eads a text file and prints it to the POSIX standard output.
+ * @filename: the file
+ * @letters: the number of letters it should read and print
+ *
+ * Return: actual number of letters it could read and print
+ *	or 0 if filename is NULL or file can be opened or read
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *alph;
+	int o = open(filename), r = read(o, alph, letter);
+	int w = write(STDOUT_FILENO, alph, r);
+
+	if (filename == NULL)
+		return (0);
+
+	alph = malloc(sizeof(char) * letters);
+
+	if (alph == NULL)
+		return (0);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+		return (0);
+
+	close(o);
+	return (w);
+}
