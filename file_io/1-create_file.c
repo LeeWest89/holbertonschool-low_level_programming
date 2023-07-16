8649c02b9ea791f83cd89f2e9f1b21ecd233e17d
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 20:24:46 -0700

    lets see if i created a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,1 +1,26 @@
-#include
+#include "main.h"
+
+/**
+ * create_textfile - creates a file.
+ * @filename: the file
+ * @text_content: text to copy
+ *
+ * Return: -1 if filename is NULL or fails
+ */
+
+int create_file(const char *filename, char *text_content);
+{
+	int o, w, a;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+		for (a = 0; text_content[a]; a++)
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	w = write(o, text_content, a)
+
+	close(o);
+	return (1);
+}
