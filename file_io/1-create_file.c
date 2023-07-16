6e9411ad984d4a5b3f0348a448d76963528f1927
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 20:28:33 -0700

    added a if statment to recognize a failure

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,10 @@ int create_file(const char *filename, char *text_content)
 		for (a = 0; text_content[a]; a++)
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC);
-	w = write(o, text_content, a)
+	w = write(o, text_content, a);
+
+	if (w == -1 || o == -1)
+		return (-1);
 
 	close(o);
 	return (1);
