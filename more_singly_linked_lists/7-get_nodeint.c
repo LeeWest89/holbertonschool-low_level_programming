36ce2fe4b3c7f86d920003d77e6429094752279b
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 23:52:22 -0700

    i forgot to include index description

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,23 @@
-#include
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index -returns the nth node of a listint_t linked list
+ * @head: pointer to thhead of the list
+ *
+ * Return: if the node does not exist, return NULL or
+ *	the nth node of a listint_t linked list.
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+{
+	unsigned int node;
+
+	for (node = 0; node < index; node++)
+	{
+		if (head == NULL)
+			return (NULL);
+
+		head = head->next;
+	}
+	return (head);
+}
