6ad7af0f0f03c410bf28a8764035bd50ae281a7c
LeeWest89 <6683@holbertonstudents.com>
2023-07-15 23:26:29 -0700

    lets see if its free

--- more_singly_linked_lists/4-free_listint.c
+++ more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,18 @@
-#include
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer to the list head
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
