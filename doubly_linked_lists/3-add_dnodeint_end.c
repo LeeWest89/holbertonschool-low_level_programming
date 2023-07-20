88d7757bee7af7bf15f8967a89390044cc1db7da
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 10:23:43 -0700

    let see if reversing works

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,29 +1,29 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
  * @head: pointer to a pointer of head
  * @n: the integer being added
  *
  * Return: the address of the new element, or NULL if it failed
  */
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_int *addonb;
+	dlistint_int *addone;
 
-	addonb = malloc(sizeof(dlistint_t));
+	addone = malloc(sizeof(dlistint_t));
 
-	if (addonb == NULL)
+	if (addone == NULL)
 		return (NULL);
 
-	addonb->prev = NULL;
-	addonb->n = n;
-	addonb->next = *head;
+	addone->next = NULL;
+	addone->n = n;
+	addone->prev = *head;
 
 	if (*head != NULL)
-		(*head)->prev = addonb;
-	*head = addonb;
+		(*head)->next = addone;
+	*head = addone;
 
-	return (addonb);
+	return (addone);
 }
