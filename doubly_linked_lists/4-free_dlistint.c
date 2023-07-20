3a69001fbafc50cd56b5ec21a494f99abee4a15a
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 10:41:36 -0700

    almost the same a 4 in singly

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,18 @@
-#include
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a list
+ * @head: the list
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
