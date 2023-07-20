be93d0976ff128beeb43bcdf3a120f87dfcc86c1
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 10:18:01 -0700

    hopefully this adds a node

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,1 +1,29 @@
-#include
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * @head: pointer to a pointer of head
+ * @n: the integer being added
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_int *addonb;
+
+	addonb = malloc(sizeof(dlistint_t));
+
+	if (addonb == NULL)
+		return (NULL);
+
+	addonb->prev = NULL;
+	addonb->n = n;
+	addonb->next = *head;
+
+	if (*head != NULL)
+		(*head)->prev = addonb;
+	*head = addonb;
+
+	return (addonb);
+}
