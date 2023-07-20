ba50829a7df6c0a19b9d321b7ce81f5b4e822214
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 11:06:55 -0700

    lets see if i find the node

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,20 @@
-#include
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: head of the list
+ * @index: the node being searched for
+ *
+ * Return: NULL if the node does not exist or nth node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+
+	for(; index != NULL; index--)
+		head = head->next;
+
+	return (head);
+}
