952f270b9d0ae7e3e3df71cf90d0b5933c1027d9
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 11:21:34 -0700

    should be the same as 8 on more_singly

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,20 @@
-#include
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data of a dlistint_t linked list.
+ * @head: pointer to the head of the list
+ *
+ * Return: the sum of the list or 0 if empty
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int add = 0;
+
+	while (head)
+	{
+		add += head->n;
+		head = head->next;
+	}
+	return (add);
+}
