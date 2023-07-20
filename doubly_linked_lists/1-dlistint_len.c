d05075993911dc065fed670acb90463dc4191c06
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 09:56:08 -0700

    think i just needed to remove the statement that prints the data

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,18 @@
-#include
+#include "lists.h"
+
+/**
+ * dlistint_len - prints number of elements of a dlistint_t list
+ * @h: the head of dlistint_t list
+ *
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	for (; h != NULL; (h = h->next))
+		nodes++;
+
+	return (nodes);
+}
