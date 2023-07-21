a1ab03e880df66eecfdbab2e0063bb5f88cc74e3
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 17:39:40 -0700

    small change lets see if it works

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,11 +13,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *temp = *h, *new_node;
 
-	new_node = malloc(sizeof(dlistint_t));
-
-	if (new_node == NULL)
-		return (NULL);
-
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -31,6 +26,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (temp->next == NULL)
 		return (add_dnodeint_end(h, n));
+	
+	new_node = malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
 
 	new_node->prev = temp;
 	new_node->n = n;
