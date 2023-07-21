cb12edbce66a58160970bab66441806844bf0e3c
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 20:23:13 -0700

    forgot to change the prototype

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,15 +1,14 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - adds an element to the hash table.
- * @ht: hash table you want to add or update the key/value to
- * @key: the key
- * @value: value associated with the key
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: hash table you want to look at
+ * @key: the key your lookin for
  *
- * Return: 1 if it succeeded, 0 otherwise
+ * Return: value associated with the element, or NULL
  */
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i = 0;
 	hash_node_t *e = NULL;
