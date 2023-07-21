c6c203d77143c8487bd84d963637af0f3b601f88
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 20:03:54 -0700

    modified 3 removed var node to make 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -1,6 +1,30 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table you want to add or update the key/value to
+ * @key: the key
+ * @value: value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
-hash_table_t *hash_table_create(unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i = 0;
+	hash_node_t *e = NULL;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	i = key_index((unsigned char *) key, ht->size);
+	e = ht->array[i];
+
+	if (e == NULL)
+		return (NULL);
+
+	if (strcmp(key, e->key) == 0)
+		e = e->next
+
+	return (e->value);
+}
