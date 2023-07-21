a3df2011d5dcb86ae0ce2594ad762575511dab06
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 21:00:45 -0700

    found some good examples, hopefully i did it right

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -8,10 +8,25 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i = 0;
+	hash_node_t *node, *temp;
+	hash_table_t *h = ht
 
 	for (; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
-			free(ht->array[i]);
+		{
+			node = ht->array[i];
+
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
 	}
+	free(h->array);
+	free(h);
 }
