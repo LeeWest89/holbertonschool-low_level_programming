600c497e76d8427e7ffbf67f9eec7996bb691d3a
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 19:03:03 -0700

    I think it will work

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,1 +1,30 @@
-#include
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: a pointer to the newly created hash table or NULL
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned int i = 0;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_table_t *) * size);
+
+	if (table->array == NULL)
+		return (NULL);
+
+	for (; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,1 +1,6 @@
-#include
+#include "hash_tables.h"
+
+/**
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
