0bd0a14c6e0bc1865041b766b0271d64300dbe83
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 17:32:38 -0700

    fix dlistint error

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t - *temp = *h, *new_node;
+	dlistint_t *temp = *h, *new_node;
 
 	new_node = malloc(sizeof(dlistint_t));
 
