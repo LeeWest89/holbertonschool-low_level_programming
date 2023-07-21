84d3e2040a38df325ced8f75e2ad411e09b1f7ab
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 19:56:56 -0700

    i hope it was a good example

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,40 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_set - adds an element to the hash table.
+ * @ht: hash table you want to add or update the key/value to
+ * @key: the key
+ * @value: value associated with the key
+ *
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
-hash_table_t *hash_table_create(unsigned long int size)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int i = 0;
+	hash_node_t *e = NULL, *node = NULL;
+
+	if (ht == NULL || key == NULL || *key =='\0' || value == NULL)
+		return (0);
+
+	i = key_index((unsigned char *) key, ht->size);
+	e = ht->array[i];
+
+	if (e && strcmp(key, e->key) == 0)
+	{
+		free(e->value);
+		e->value = strdup(value);
+		return (1);
+	}
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[i];
+	ht->array[i] = node;
+
+	return (1);
+}
