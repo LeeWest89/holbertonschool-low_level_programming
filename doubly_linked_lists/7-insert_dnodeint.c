cae298e93f46b954d67040a5f0248bd28e67f1b8
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 17:56:57 -0700

    fixed compiler issue i hope

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -46,7 +46,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->next = temp->next;
 
 	if (temp->next)
-		temp->->prev = new_node;
+		temp->next->prev = new_node;
 	temp->next = new_node;
 
 	return (new_node);
