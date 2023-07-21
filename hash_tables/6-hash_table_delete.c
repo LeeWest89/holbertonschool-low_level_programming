1836bbc8866f7ba05f07af623b7a1bf535053796
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 20:49:45 -0700

    might be this simple

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,6 +1,17 @@
 #include "hash_tables.h"
 
 /**
+ * hash_table_delete - deletes the hash table
+ * @ht: the hash table
  */
 
-hash_table_t *hash_table_create(unsigned long int size)
+void hash_table_delete(hash_table_t *ht);
+{
+	unsigned long int i = 0;
+
+	for (; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+			free(ht->array[i]);
+	}
+}
