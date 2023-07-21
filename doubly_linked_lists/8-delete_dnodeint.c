c43496659b3027244e44b30a437665e5182fbfe6
LeeWest89 <6683@holbertonstudents.com>
2023-07-20 18:11:13 -0700

    similar to my orginal 7 i hope it works ->next->prev make my head hurt

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -1,1 +1,41 @@
-#include
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at index in the list.
+ * @head: pointer to the pointer of the head
+ * @index: index of the node that should be deleted.
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp = *head;
+
+	if (temp == NULL)
+		return (-1);
+
+	for (; index != 0; index--)
+	{
+		if (temp == NULL)
+			return (-1);
+		temp = temp->next;
+	}
+
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (head != NULL)
+			(*head)->prev = NULL;
+	}
+
+	else
+	{
+		temp->prev->next = temp->next;
+		if (temp->next != NULL)
+			temp->next->prev = temp->prev;
+	}
+
+	free(temp);
+	return (1);
+}
