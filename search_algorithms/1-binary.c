0a49de4a577a23361fb4ac2f2ba76c2a8668af94
LeeWest89 <6683@holbertonstudents.com>
2023-08-18 13:26:45 -0700

    figured how to do it recursively i think

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,21 +1,56 @@
-#include "main.h"
+#include "search_algos.h"
 
 /**
- * linear_search - searches for a value in an array of integers
+ * binary_search_r - uses recursion to execute a binary search
+ *
+ * @array: the array being searched
+ * @low: start of the array
+ * @high: end of array
+ * @value: value we are searching for
+ *
+ * Return: the value
+ */
+
+int binary_search_r(int *array, int low, int high, int value)
+{
+	int i, mid = low + (high - low) / 2;
+
+	printf("Searching in array: ");
+	for (i = low; i <= high; i++)
+	{
+		printf("%d", array[i]);
+		if (i < high)
+			printf(", ");
+	}
+	printf("\n");
+
+	if (low > high)
+		return (-1);
+
+	if (array[mid] == value)
+			return (mid);
+
+	else if (array[mid] > value)
+		return binary_search_r(array, low, (mid - 1), value);
+
+	else
+		return binary_search_r(array, (mid + 1), high, value);
+}
+
+/**
+ * binary_search - searches for a value in an array of integers
  *
  * @array: the array to search
  * @size: the size of the array
  * @value: the value being searched for
+ *
+ * Return: i if values is found or -1 if NULL or value is not in array
  */
 
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	int i = 0;
 
-	for (; i < size; i++)
-	{
-		if (array[i] == value)
-			return (i);
-	}
-	return (-1);
+	if (array == NULL)
+		return (-1);
+	return binary_search_r(array, 0, size - 1, value);
 }
--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -5,6 +5,7 @@
 #include <stddef.h>
 
 int linear_search(int *array, size_t size, int value);
+int binary_search_r(int *array, int low, int high, int value);
 int binary_search(int *array, size_t size, int value);
 
 #endif
