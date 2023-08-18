879ac824e9f30f6d05ffb7429795d8ea659acef7
LeeWest89 <6683@holbertonstudents.com>
2023-08-18 11:48:07 -0700

    simple fix to header

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,14 +6,20 @@
  * @array: the array to search
  * @size: the size of the array
  * @value: the value being searched for
+ *
+ * Return: i if values is found or -1 if NULL or value is not in array
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
 
 	for (; i < size; i++)
 	{
+		printf("Value checked array[%ld] = [%i]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
