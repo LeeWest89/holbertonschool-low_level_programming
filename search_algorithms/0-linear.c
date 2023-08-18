422ce2ed603198735e0cec47afc80c49e319400f
LeeWest89 <6683@holbertonstudents.com>
2023-08-18 10:46:53 -0700

    fix personal error lets see if it works

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ *
+ * @array: the array to search
+ * @size: the size of the array
+ * @value: the value being searched for
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i = 0;
+
+	for (; i < size; i++)
+	{
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
